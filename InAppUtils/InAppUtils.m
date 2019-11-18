4cfef96c20bc4306d4f9d19aee5ed5b559035eb9
Maxime Sioui <maxime@kreezee.com>
2019-11-18 11:42:27 -0500

    Pass code definition to error callback

--- InAppUtils/InAppUtils.m
+++ InAppUtils/InAppUtils.m
@@ -118,57 +118,8 @@ restoreCompletedTransactionsFailedWithError:(NSError *)error
 {
     NSString *key = RCTKeyForInstance(@"restoreRequest");
     RCTResponseSenderBlock callback = _callbacks[key];
-    if (callback) {
-        switch (error.code)
-        {
-            case SKErrorUnknown: 
-                callback(@[@"error_unknown"]);
-                break;
-            case SKErrorClientInvalid: 
-                callback(@[@"client_invalid"]);
-                break;
-            case SKErrorPaymentCancelled:
-                callback(@[@"user_cancelled"]);
-                break;
-            case SKErrorPaymentInvalid: 
-                callback(@[@"payment_invalid"]);
-                break;
-            case SKErrorPaymentNotAllowed: 
-                callback(@[@"payment_not_allowed"]);
-                break;
-            case SKErrorStoreProductNotAvailable:
-                callback(@[@"store_product_not_available"]);
-                break;
-            case SKErrorCloudServicePermissionDenied: 
-                callback(@[@"cloud_service_permission_denied"]);
-                break;
-            case SKErrorCloudServiceNetworkConnectionFailed: 
-                callback(@[@"cloud_service_network_connection_failed"]);
-                break;
-            case SKErrorCloudServiceRevoked:
-                callback(@[@"cloud_service_revoked"]);
-                break;
-            case SKErrorPrivacyAcknowledgementRequired: 
-                callback(@[@"privacy_acknowledgement_required"]);
-                break;
-            case SKErrorUnauthorizedRequestData: 
-                callback(@[@"unauthorized_request_data"]);
-                break;
-            case SKErrorInvalidOfferIdentifier:
-                callback(@[@"invalid_offer_identifier"]);
-                break;
-            case SKErrorInvalidOfferPrice: 
-                callback(@[@"invalid_offer_price"]);
-                break;
-            case SKErrorInvalidSignature: 
-                callback(@[@"invalid_signature"]);
-                break;
-            case SKErrorMissingOfferParams:
-                callback(@[@"missing_offer_params"]);
-                break;
-            default:
-                callback(@[@"restore_failed"]);
-                break;
+    if (callback) {        
+            callback(@[NSString stringWithFormat: @"Error: Code=%@ Domain=%@", error.code, error.domain]);
         }
 
         [_callbacks removeObjectForKey:key];
